b030a951cb8be592ca50ba0d0586945c15a41b59
Aphaki <kiapha1108@gmail.com>
2022-06-08 15:39:04 +0900

    Initial Commit

---
+++ USDExchangeRateAPI/USDExchangeRateAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  USDExchangeRateAPIApp.swift
+//  USDExchangeRateAPI
+//
+//  Created by Sy Lee on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct USDExchangeRateAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
